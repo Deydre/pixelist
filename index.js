d0f77eb780ea2cb71c615baf98e753f3e9e8ac93
Deydre <deydrealonso@gmail.com>
2024-11-23 20:47:03 +0100

    add/inicial structure

---
+++ index.js
@@ -0,0 +1,23 @@
+// Inicializar el servidor con express
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//Logger
+const morgan = require("./middlewares/morgan")
+app.use(morgan(':method :url :status - :response-time ms :body'));
+
+// Habilitar la recepción de peticiones tipo JSON
+app.use(express.json());
+
+// Rutas
+// ...
+
+// Para ruta no existente
+app.use("*", (req, res) => {
+    res.status(404).send("Ruta no encontrada");
+})
+
+app.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}`)
+});
