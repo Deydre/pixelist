1fbf2b795a331d388e0f067021f8b2222fd1f72b
Deydre <deydrealonso@gmail.com>
2024-11-28 16:52:31 +0100

    add/pagination and connexion of home and game detail

--- client/src/components/Main/Game/Game.jsx
+++ client/src/components/Main/Game/Game.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { useParams } from "react-router-dom";
+import CategoriesBar from "../../commons/CategoriesBar/CategoriesBar";
+import SearchBar from "../../commons/searchBar";
+import DetailCard from "./DetailCard/DetailCard"
+import axios from "axios";
 
 const GameView = () => {
-  return <div>GameView</div>;
+
+  const routeParams = useParams();
+  const apiKey = import.meta.env.VITE_API_KEY;
+  const [loading, setLoading] = useState(true);
+  const [gameData, setGameData] = useState([]);
+
+  const renderParamsCard = () => {
+    return <DetailCard data={gameData} key="1" />
+  }
+
+  // FETCH DEL PARAM
+  useEffect(() => {
+    const getGame = async () => {
+      try {
+        const resp = await axios.get(`https://api.rawg.io/api/games/${routeParams.game}?key=${apiKey}`);
+        setGameData(resp.data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    getGame();
+  }, []);
+
+  return <section id="game">
+    <CategoriesBar />
+    <div id="contentGame">
+      <SearchBar />
+      <section id="contentCard">
+        {renderParamsCard()}
+      </section>
+    </div>
+  </section>;
 };
 
 export default GameView;
--- client/src/components/Main/Home/Home.jsx
+++ client/src/components/Main/Home/Home.jsx
@@ -1,15 +1,19 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import CategoriesBar from "../../commons/CategoriesBar/CategoriesBar";
 import SearchBar from "../../commons/searchBar";
 import Card from "../../commons/Card";
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
+import HashLoader from "react-spinners/HashLoader";
 import axios from "axios";
 import { v4 as uuidv4 } from 'uuid';
 
-// import PageContent from 
 const Home = () => {
 
   const apiKey = import.meta.env.VITE_API_KEY;
   const [videogamesList, setVideogamesList] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(true);
 
   const renderCards = () => {
     return videogamesList.map((game, i) => <Card data={game} key={uuidv4()} />)
@@ -18,23 +22,50 @@ const Home = () => {
   // FETCH INICIAL
   useEffect(() => {
     const getVideogames = async () => {
+      setLoading(true);
       try {
-        const resp = await axios.get(`https://api.rawg.io/api/games?page=43871&key=${apiKey}`);
-        console.log(resp.data.results)
-        setVideogamesList(resp.data.results)
+        const resp = await axios.get(`https://api.rawg.io/api/games?page=${page}&key=${apiKey}`);
+        setVideogamesList(resp.data.results);
       } catch (err) {
         console.log(err)
+      } finally {
+        setLoading(false);
       }
     }
     getVideogames();
-  }, []);
+  }, [page]);
+
+  const count = 43871;
 
   return <section id="home">
-    <CategoriesBar/>
+    <CategoriesBar />
     <div id="contentHome">
-      <SearchBar/>
+      <SearchBar />
       <section id="contentCards">
-        {renderCards()}
+        { loading ? <HashLoader color="#fff"/> : renderCards() }
+      </section>
+      <section id="pagination">
+        <Stack spacing={2}>
+          <Pagination
+            count={count}
+            shape="rounded"
+            page={page}
+            onChange={(event, value) => setPage(value)}
+            sx={{ // Estilos para el componente de MUI
+              '& .MuiPaginationItem-root': {
+                color: 'rgba(255, 255, 255, 0.3)',
+                '&:hover': {
+                  backgroundColor: 'rgba(255, 255, 255, 0.10)',
+                  padding: 'inherit',
+                },
+              },
+              '& .Mui-selected': {
+                backgroundColor: 'rgba(255, 255, 255, 0.179)',
+                color: '#fff',
+              },
+            }}
+          />
+        </Stack>
       </section>
     </div>
   </section>;
