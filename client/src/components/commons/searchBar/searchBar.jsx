665352068cac218d0de5beddd3fe69d3c900fa04
Deydre <deydrealonso@gmail.com>
2024-11-28 09:08:15 +0100

    add/view of home

--- client/src/components/commons/searchBar/searchBar.jsx
+++ client/src/components/commons/searchBar/searchBar.jsx
@@ -2,24 +2,28 @@ import React from "react";
 import { useContext, useState } from "react";
 
 const SearchBar = () => {
-  
-const [values, setValue] = useState("");
 
-const handleChange = (e) => {
-  setValue(e.target.value);
-}
+  const [values, setValue] = useState("");
 
-const handleSubmit = (e) => {
-  e.preventDefault()
-  // Enviamos la búsqueda al estado "pokemons" del contexto a través de la función del contexto para que se sume a ese array
-  // updatePokemons(values);
-  setValue("");
-}
-  return <>
-    <form id= "searchOne" onSubmit={handleSubmit}>
-      <input type="text" name="nombre" value={values} onChange={handleChange} placeholder="🔍   Search a videogame!" required/>
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // Enviamos la búsqueda al estado "pokemons" del contexto a través de la función del contexto para que se sume a ese array
+    // updatePokemons(values);
+    setValue("");
+  }
+  return <div id="contentSearch">
+    <form onSubmit={handleSubmit}>
+      <input type="text" name="nombre" value={values} onChange={handleChange} placeholder="🔍   Search a videogame!" required />
     </form>
-  </>;
+    <div id="sortButtons">
+      <button id="sortAlphabetical" class="btnSort">A</button>
+      <button id="sortNewest" class="btnSort">n</button>
+    </div>
+  </div>;
 };
 
 export default SearchBar;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const cors = require('cors');
 
 //Logger
 const morgan = require("./middlewares/morgan")
@@ -9,6 +10,7 @@ app.use(morgan(':method :url :status - :response-time ms :body'));
 
 // Habilitar la recepción de peticiones tipo JSON
 app.use(express.json());
+app.use(cors());
 
 // Rutas importadas
 const userRoutes = require("./routes/user.routes");
