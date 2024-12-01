d80ba4236b6a3dff4212e62a8a00f4555d6e8020
Deydre <75368782+Deydre@users.noreply.github.com>
2024-12-01 12:21:27 +0100

    Revert "Feature/cards info"

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const cors = require('cors');
-const path = require('path');
 
 //Logger
 const morgan = require("./middlewares/morgan")
@@ -11,36 +9,14 @@ app.use(morgan(':method :url :status - :response-time ms :body'));
 
 // Habilitar la recepción de peticiones tipo JSON
 app.use(express.json());
-app.use(cors({
-    origin: 'http://localhost:5173', // URL del front
-    credentials: true
-}));
 
-// Rutas importadas
-const userRoutes = require("./routes/user.routes");
-const videogameRoutes = require("./routes/videogame.routes");
-const favoritesRoutes = require("./routes/favorites.routes");
-const wishlistedRoutes = require("./routes/wishlisted.routes");
-const completedRoutes = require("./routes/completed.routes");
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-// Rutas habilitadas
-app.use('/api/user', userRoutes);
-app.use('/api/videogame', videogameRoutes);
-app.use('/api/favorites', favoritesRoutes);
-app.use('/api/wishlisted', wishlistedRoutes);
-app.use('/api/completed', completedRoutes);
-
-app.get("*", (req, res) => { res.sendFile(path.join(__dirname + '/client/build/index.html')) });
-
+// Rutas
+// ...
 
 // Para ruta no existente
 app.use("*", (req, res) => {
     res.status(404).send("Ruta no encontrada");
 })
-
 
 app.listen(port, () => {
     console.log(`Listening on http://localhost:${port}`)
--- routes/videogame.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-// Rutas de usuario
-const videogameController = require("../controllers/videogame.controller");
-const router = express.Router();
-
-router.get('/:id', videogameController.checkVideogameExistsById);
-router.post('/', videogameController.createVideogame);
-
-module.exports = router;
