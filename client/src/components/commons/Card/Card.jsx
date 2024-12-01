cee2cb1a150912a625893118f7d7f54c8a41664d
Deydre <deydrealonso@gmail.com>
2024-12-01 09:28:48 +0100

    minor fixes

--- client/src/components/commons/Card/Card.jsx
+++ client/src/components/commons/Card/Card.jsx
@@ -3,9 +3,16 @@ import { useNavigate } from "react-router-dom";
 
 const Card = (game) => {
   const navigate = useNavigate();
-  const { name, background_image, id, metacritic, slug, description } = game.data;
+  console.log(game.data)
+  const { name, background_image, id, metacritic, slug, released, parent_platforms } = game.data;
+
   let colorMetacritic;
   metacritic >= 75 ? colorMetacritic = "green" : metacritic >= 50 ? colorMetacritic = "yellow" : colorMetacritic = "red";
+
+  let arrayPlatforms = parent_platforms.map(platform => platform.name)
+  const renderPlatforms = () => {
+    return arrayPlatforms.map((platform, i) => <p>Hola</p>)
+  }
 
   const details = () => {
     navigate(`/games/${slug}`)
@@ -23,7 +30,10 @@ const Card = (game) => {
             <h6>{metacritic}</h6>
           </div>
         </div>
-        <p>Poner más cosas</p>
+        <div>
+          <p><strong>Released:</strong> {released}</p>
+          {renderPlatforms}
+        </div>
       </div>
     </article>
   </>;
